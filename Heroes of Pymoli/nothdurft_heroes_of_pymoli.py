24f329a4e372c98f3a98b7ac2adbaab737d47803
Nick Nothdurft <nothdurftnick@gmail.com>
2018-09-24 21:16:18 -0500

    Update Homework

--- Heroes of Pymoli/nothdurft_heroes_of_pymoli.py
+++ Heroes of Pymoli/nothdurft_heroes_of_pymoli.py
@@ -4,7 +4,7 @@ gameData = pd.read_csv(filePath)
 
 #Number of Players
 totPlayers = pd.DataFrame({"Total Players": gameData["SN"].nunique()}, index=[0])
-print(totPlayers)
+totPlayers
 
 #Number of unique items
 items = gameData["Item ID"].nunique()
@@ -13,7 +13,7 @@ purchases = gameData["Purchase ID"].nunique()
 totRev = "${:,.2f}".format(gameData["Price"].sum())
 purchAnalysis = {"Number of Unique Items": items, "Average Price": avgPrice, "Number of Purchases": purchases, "Total Revenue": totRev}
 pa_df = pd.DataFrame(data=purchAnalysis, index=[0])
-print(pa_df)
+pa_df
 
 #Gender Demographics
 gender = gameData.drop_duplicates("SN")
@@ -23,7 +23,7 @@ other = gender["Gender"][gender.Gender == "Other / Non-Disclosed"].count()
 total = males + females + other
 genderDemo = {"Percentage of Players": ["{:.2f}%".format((males/total)*100), "{:.2f}%".format((females/total)*100), "{:.2f}%".format((other/total)*100)], "Total Count":[males, females, other]}
 genderDemo = pd.DataFrame(genderDemo, index=["Male", "Female", "Other / Non-Disclosed"])
-print(genderDemo)
+genderDemo
 
 
 #Purchasing Analysis (Gender)
@@ -39,7 +39,6 @@ oSum = gameData["Price"][gameData.Gender == "Other / Non-Disclosed"].sum()
 genderPurch = {"Gender": ["Female", "Male", "Other / Non-Disclosed"], "Purchase Count": [femalePurch, malePurch, otherPurch], "Average Purchase Price": ["${:.2f}".format(fPrice), "${:.2f}".format(mPrice), "${:.2f}".format(oPrice)], "Total Purchase Value": ["${:.2f}".format(fSum), "${:.2f}".format(mSum), "${:.2f}".format(oSum)], "Avg Purchase Total per Person": ["${:.2f}".format((fSum/femalePurch)), "${:.2f}".format((mSum/malePurch)), "${:.2f}".format((oSum/otherPurch))]}
 genderPurch = pd.DataFrame(genderPurch)
 genderPurch.set_index("Gender")
-print(genderPurch)
 
 #Age Demographics
 age_bins = [0, 9.90, 14.90, 19.90, 24.90, 29.90, 34.90, 39.90, 99999]
@@ -50,7 +49,7 @@ SN = sum_df.join(temp_df).groupby("Age Groups").count()["SN"]
 grp = ["{:.2f}%".format(x / SN.sum() * 100) for x in SN]
 ageDemo = {"Percentage of Players": grp, "Total Count": SN}
 ageDemo = pd.DataFrame(ageDemo, index=group_names)
-print(ageDemo)
+ageDemo
 
 temp_df = pd.DataFrame({"Age Groups": pd.cut(gameData["Age"], age_bins, labels=group_names)})
 purchCount = gameData.join(temp_df).groupby("Age Groups").count()["Price"]
@@ -63,7 +62,7 @@ purchAnalysisAge = pd.DataFrame({"Purchase Count": purchCount,
 purchAnalysisAge["Average Purchase Price"] = purchAnalysisAge["Average Purchase Price"].apply("${:.2f}".format)
 purchAnalysisAge["Total Purchase Value"] = purchAnalysisAge["Total Purchase Value"].apply("${:.2f}".format)
 purchAnalysisAge["Average Purchase Total per Person"] = purchAnalysisAge["Average Purchase Total per Person"].apply("${:.2f}".format)
-print(purchAnalysisAge)
+purchAnalysisAge
 
 screenNames = gameData.groupby("SN").count()
 screenNames = screenNames.drop(columns=["Age", "Gender", "Item ID", "Item Name", "Price"])
@@ -74,7 +73,7 @@ playerPurchases = pd.merge(screenNames, tpv, on="SN")
 playerPurchases = playerPurchases.sort_values(by="Price", ascending=False)
 playerPurchases = playerPurchases.rename(columns={"Price": "Total Purchase Value"})
 playerPurchases["Total Purchase Value"] = playerPurchases["Total Purchase Value"].map("${:.2f}".format)
-print(playerPurchases.head())
+playerPurchases.head()
 
 totalPurchaseValue = gameData.groupby(["Item ID", "Item Name"])["Price"].sum()
 purchaseCount = gameData.groupby(["Item ID", "Item Name"])["Purchase ID"].count()
@@ -83,9 +82,9 @@ popularItems = pd.DataFrame({"Purchase Count": purchaseCount, "Item Price": item
 profitableItems = popularItems.copy()
 popularItems["Item Price"] = popularItems["Item Price"].map("${:.2f}".format)
 popularItems["Total Purchase Value"] = popularItems["Total Purchase Value"].map("${:.2f}".format)
-print(popularItems.sort_values(by="Purchase Count", ascending=False).head())
+popularItems.sort_values(by="Purchase Count", ascending=False).head()
 
 profitableItems = profitableItems.sort_values(by="Total Purchase Value", ascending=False)
 profitableItems["Item Price"] = profitableItems["Item Price"].map("${:.2f}".format)
 profitableItems["Total Purchase Value"] = profitableItems["Total Purchase Value"].map("${:.2f}".format)
-print(profitableItems.head())
+profitableItems.head()
